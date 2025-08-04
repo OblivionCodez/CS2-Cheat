1c8d5c0e72d363bee7b1bd1360e65c9f3be8a81c
Oblivion <kailynjoshuabadenarmstrong18@gmail.com>
2025-08-05 00:13:52 +0100

    Create cheat.cs

---
+++ cheat.cs
@@ -0,0 +1,11 @@
+namespace Cheat
+{
+    internal static class CheatClass
+    {
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Not cheating");
+        }
+    }
+}
